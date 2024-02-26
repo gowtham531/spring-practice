a5c83f2b230f953114b32be8c115827c854c4378
gowtham <gowthamkosuri1@gmail.com>
2024-02-26 17:25:15 +0530

    NEWBRANCH COMMIT

---
+++ spring-rest-services/src/main/java/com/restservices/gitcheck/Add.java
@@ -0,0 +1,9 @@
+package com.restservices.gitcheck;
+
+public class Add {
+	public int sun(int x,int y) {
+		int result = x+y;
+		return result;
+	}
+
+}
---
+++ spring-rest-services/src/main/java/com/restservices/gitcheck/ArthematicFunctions.java
@@ -0,0 +1,12 @@
+package com.restservices.gitcheck;
+
+public class ArthematicFunctions {
+
+	public static void main(String[] args) {
+		Add add = new Add();
+		int result = add.sun(10, 20);
+		System.out.println("sum is::"+result);
+
+	}
+
+}
